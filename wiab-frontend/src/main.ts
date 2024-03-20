1a8ff63f80303a23f7716a854b272d517ababaab
Jakob Dahlberg <dahlbergjakob@hotmail.com>
2024-03-20 20:41:40 +0100

    Fix LandingPage component.
    Install & Add FontAwesomeIcon.
    
    Added three pictures in assets.

--- wiab-frontend/src/main.ts
+++ wiab-frontend/src/main.ts
@@ -1,6 +1,14 @@
 import { createApp } from "vue";
 import "./style.css";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faStop } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import App from "./App.vue";
 import { router } from "./router";
 
-createApp(App).use(router).mount("#app");
+library.add(faStop);
+
+createApp(App)
+  .use(router)
+  .component("font-awesome-icon", FontAwesomeIcon)
+  .mount("#app");
