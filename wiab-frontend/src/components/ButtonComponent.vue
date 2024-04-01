e0c01e98ee64d0b74887afbf123ad95db4b3940d
Jakob Dahlberg <dahlbergjakob@hotmail.com>
2024-04-01 21:59:51 +0200

    feat: add reuseable button component with 3 different sizes.
    all of them renderd in Certificate Page atm.
    
    fix: add 2 new custom styles. Black & Green.

---
+++ wiab-frontend/src/components/ButtonComponent.vue
@@ -0,0 +1,72 @@
+<script setup lang="ts">
+import { computed } from "vue";
+type ButtonProps = {
+  /**
+   * @description The color of the button
+
+   */
+
+  color: "white" | "black";
+
+  /**
+   * @description The function to be called when the button is clicked
+
+   */
+
+  onClick: () => void;
+
+  /**
+   * @description The size of the button
+
+   */
+
+  size: "small" | "medium" | "large";
+
+  /**
+   * @description The text to be displayed on the button
+
+   */
+
+  text: string;
+};
+
+const props = defineProps<ButtonProps>();
+
+const sizeClasses = {
+  small: "min-w-24 h-10 text-sm",
+  medium: "min-w-28 h-12 text-md",
+  large: "min-w-32 h-14 text-lg",
+};
+
+const colorClasses = {
+  white: "bg-white text-black hover:bg-custom-green hover:text-white",
+  black: "bg-custom-black text-white hover:bg-custom-green hover:text-white",
+};
+
+const buttonClasses = computed(() => [
+  "flex",
+  "items-center",
+  "justify-center",
+  "transition",
+  "duration-300",
+  "ease-in-out",
+  "transform",
+  "hover:scale-110",
+  "hover:shadow-lg",
+  "whitespace-nowrap",
+  "p-4",
+  "rounded",
+  "shadow-md",
+  "font-serif",
+  sizeClasses[props.size],
+  colorClasses[props.color],
+]);
+</script>
+
+<template>
+  <button :class="buttonClasses" @click="props.onClick">
+    {{ props.text }}
+  </button>
+</template>
+
+<style setup></style>
--- wiab-frontend/tailwind.config.js
+++ wiab-frontend/tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "custom-green": "#009087",
+        "custom-black": "#323643",
+      },
+    },
   },
   plugins: [],
 };
